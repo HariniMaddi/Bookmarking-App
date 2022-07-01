0151f1d5f03ea9df0262d71a180797498f9433a9
HariniMaddi <66894404+HariniMaddi@users.noreply.github.com>
2022-07-02 05:04:42 +0530

    Add files via upload
    Using JDBC

--- thrillio/src/com/semanticsquare/thrillio/View.java
+++ thrillio/src/com/semanticsquare/thrillio/View.java
@@ -1,18 +1,32 @@
 package com.semanticsquare.thrillio;
 
+import java.util.List;
+
 import com.semanticsquare.thrillio.controllers.BookmarkController;
 import com.semanticsquare.thrillio.entities.Bookmark;
 import com.semanticsquare.thrillio.entities.User;
 
 public class View {
-	public static void bookmark(User user,Bookmark[][] bookmarks) {
-		System.out.println(user.getEmail()+"is bookmarking");
-		for(int i=0;i<DataStore.USER_BOOKMARK_LIMIT;i++) {
-			int typeOffset = (int)(Math.random()*DataStore.BOOKMARK_TYPES_COUNT);
-			int bookmarkOffset=(int)(Math.random()*DataStore.BOOKMARK_COUNT_PER_TYPE);
-			Bookmark bookmark=bookmarks[typeOffset][bookmarkOffset];
-			BookmarkController.getInstance().saveUserBookmark(user,bookmark);
-			System.out.println(bookmark);
+	private static boolean getBookmarkDecision(Bookmark bookmark) {
+		return Math.random()<0.5 ? true:false;
+	}
+	public static void bookmark(User user,List<List<Bookmark>>bookmarks) {
+		
+		System.out.println("\n"+user.getEmail()+" is browsing items..");
+		int bookmarkCount=0;
+		
+		for(List<Bookmark> bookmarkList : bookmarks) {
+			for(Bookmark bookmark : bookmarkList) {
+				boolean isBookmarked=getBookmarkDecision(bookmark);
+				
+				if(isBookmarked) {
+					bookmarkCount++;
+					
+					BookmarkController.getInstance().saveUserBookmark(user, bookmark);
+					System.out.println("New item Bookmarked");
+				}
+			}
+		
 		}
 	}
 
--- thrillio/src/com/semanticsquare/thrillio/dao/BookmarkDao.java
+++ thrillio/src/com/semanticsquare/thrillio/dao/BookmarkDao.java
@@ -1,17 +1,65 @@
 package com.semanticsquare.thrillio.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
 import com.semanticsquare.thrillio.DataStore;
+import com.semanticsquare.thrillio.entities.Book;
 import com.semanticsquare.thrillio.entities.Bookmark;
 import com.semanticsquare.thrillio.entities.UserBookmark;
 
 public class BookmarkDao {
-public  Bookmark[][] getBookmarks() {
+public  List<List<Bookmark>>   getBookmarks() {
 	return DataStore.getBookmarks();
 }
 
 public void saveUserBookmark(UserBookmark userBookmark) {
 	// TODO Auto-generated method stub
-	DataStore.add(userBookmark);
+	//DataStore.add(userBookmark);
+	try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jid_thrillio?useSSL=false", "root", "root");
+			Statement stmt = conn.createStatement();) {
+		if(userBookmark.getBookmark() instanceof Book) {
+			saveUserBook(userBookmark,stmt);
+		}
+		else if(userBookmark.getBookmark() instanceof Movie) {
+			saveUserMovie(userBookmark,stmt);
+		}
+		else {
+			saveUserWebLink(userBookmark,stmt);
+		}
+		
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+	
+}
+
+private void saveUserWebLink(UserBookmark userBookmark, Statement stmt) throws SQLException {
+	// TODO Auto-generated method stub
+	String query="insert into User_WebLink (user_id,movie_id) values (" +
+			userBookmark.getUser().getId()+" ,"+userBookmark.getBookmark().getId();
+			stmt.executeUpdate(query);
+			
+	
+}
+
+private void saveUserMovie(UserBookmark userBookmark, Statement stmt) throws SQLException {
+	// TODO Auto-generated method stub
+	String query="insert into User_Movie (user_id,movie_id) values (" +
+	userBookmark.getUser().getId()+" ,"+userBookmark.getBookmark().getId();
+	stmt.executeUpdate(query);
+			
+	
+}
+
+private void saveUserBook(UserBookmark userBookmark, Statement stmt) throws SQLException {
+	// TODO Auto-generated method stub
+	String query="insert into User_Book (user_id,book_id) values (" +
+	userBookmark.getUser().getId()+" ,"+userBookmark.getBookmark().getId();
+	stmt.executeUpdate(query);
 	
 }
 	
